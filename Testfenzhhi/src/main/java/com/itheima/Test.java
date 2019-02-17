83dce88535e4cc6509c04572bc7e4c71c3ec63ae
刘畅 <lc199539@live.com>
2019-02-17 21:49:02 +0800

    更新master

--- Testfenzhhi/src/main/java/com/itheima/Test.java
+++ Testfenzhhi/src/main/java/com/itheima/Test.java
@@ -5,4 +5,9 @@ public class Test {
     public static void findAll(){
         System.out.println("静态方法");
     }
+
+    public static void findByPwd(){
+        System.out.println("master分支");
+    }
+
 }
