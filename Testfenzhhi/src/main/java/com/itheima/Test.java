b967cf364aeb916f79cb03f08f1cb25be67c065c
刘畅 <lc199539@live.com>
2019-02-17 21:45:51 +0800

    更新dev分支

--- Testfenzhhi/src/main/java/com/itheima/Test.java
+++ Testfenzhhi/src/main/java/com/itheima/Test.java
@@ -5,4 +5,8 @@ public class Test {
     public static void findAll(){
         System.out.println("静态方法");
     }
+
+    public static void findByName(){
+        System.out.println("dev1分支");
+    }
 }
